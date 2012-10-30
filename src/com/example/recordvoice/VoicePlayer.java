b9b331463c3b1b53b255d6b1ecde3e8f888bc03e
smnksg <smnksg-2012@yahoo.co.jp>
2012-10-30 18:07:40 +0900

    first commit

---
+++ src/com/example/recordvoice/VoicePlayer.java
@@ -0,0 +1,49 @@
+package com.example.recordvoice;
+
+import java.io.IOException;
+
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.os.Environment;
+import android.util.Log;
+
+public class VoicePlayer implements OnCompletionListener {
+	private MediaPlayer mPlayer;
+	private String mFilePath;	//ファイルパス
+
+	public VoicePlayer() {
+		// インスタンスの取得
+        mPlayer = new MediaPlayer();
+        // Listenerの登録必要です
+        mPlayer.setOnCompletionListener(this);
+		// SDカードのディレクトリ
+		mFilePath = Environment.getExternalStorageDirectory().toString()
+    				+ "/RecordVoice/testVoice.3gp";
+	}
+	
+	//音声ファイル再生
+	public void play() {
+        try {
+            mPlayer.setDataSource(mFilePath);
+            // 準備して
+            mPlayer.prepare();
+            // 再生スタート！
+            mPlayer.start();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+	public void onCompletion(MediaPlayer paraPlayer) {
+		// 再生を停止して
+        mPlayer.stop();
+        //mPlayer.reset();
+        //mPlayer.setOnCompletionListener(null);
+        // メモリの解放
+        mPlayer.release();
+        //mPlayer=null;
+        Log.v("VoicePlayer", "onCompletion is Called");
+	}
+}
